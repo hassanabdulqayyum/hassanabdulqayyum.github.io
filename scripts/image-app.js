65d0ff2514ff280d50bdffa39faa360efc5ad0ad
hassan <hassan@hassans-MacBook-Pro.local>
2015-08-13 17:09:05 +0500

    reverted

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -41,9 +41,19 @@
     toggleButtonsAbledness();
 
     var myWorker = new Worker("scripts/worker.js");
-    myWorker.onmessage(imageData);
-    imageData = myWorker.postMessage();
 
+    length = imageData.data.length / 4;
+    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+      r = imageData.data[i * 4 + 0];
+      g = imageData.data[i * 4 + 1];
+      b = imageData.data[i * 4 + 2];
+      a = imageData.data[i * 4 + 3];
+      pixel = manipulate(type, r, g, b, a);
+      imageData.data[i * 4 + 0] = pixel[0];
+      imageData.data[i * 4 + 1] = pixel[1];
+      imageData.data[i * 4 + 2] = pixel[2];
+      imageData.data[i * 4 + 3] = pixel[3];
+    }
     toggleButtonsAbledness();
     return ctx.putImageData(imageData, 0, 0);
   };
