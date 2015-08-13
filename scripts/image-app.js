d36270e5fc567f5561e7163601cb64d71b3219fc
hassan <hassan@hassans-MacBook-Pro.local>
2015-08-13 19:50:03 +0500

    more logs

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -37,7 +37,6 @@
   function manipulateImage(type) {
     var a, b, g, i, imageData, j, length, pixel, r, ref;
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    console.log(type);
 
     toggleButtonsAbledness();
 
