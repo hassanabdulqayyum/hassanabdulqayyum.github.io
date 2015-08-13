4aa1784a6d8b4d1aa24c5fce3c952b32f9398ba9
hassan <hassan@hassans-MacBook-Pro.local>
2015-08-13 20:46:07 +0500

    changed logs again

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,8 +1,8 @@
 importScripts('imageManips.js');
 
 this.onmessage = function(e) {
-  console.log(e);
-  console.log(e.data.data);
+  console.log(e.data);
+  console.log(e.data.data.data);
   var imageData = e.data.data;
   var type = e.type;
 
