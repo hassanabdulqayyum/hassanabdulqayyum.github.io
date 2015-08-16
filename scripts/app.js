3f39fa8ef8210540dcacb7287f88363bdebd6d0b
hassan <hassan@hassans-MacBook-Pro.local>
2015-08-16 22:27:02 +0500

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -255,8 +255,6 @@ APP.Main = (function() {
   function colorizeAndScaleStories() {
 
     var storyElements = document.querySelectorAll('.story');
-    var height = main.offsetHeight;
-    var mainPosition = main.getBoundingClientRect();
 
     // It does seem awfully broad to change all the
     // colors every time!
@@ -267,7 +265,8 @@ APP.Main = (function() {
       var title = story.querySelector('.story__title');
 
       // Base the scale on the y position of the score.
-      
+      var height = main.offsetHeight;
+      var mainPosition = main.getBoundingClientRect();
       var scoreLocation = score.getBoundingClientRect().top -
           document.body.getBoundingClientRect().top;
       var scale = Math.min(1, 1 - (0.05 * ((scoreLocation - 170) / height)));
@@ -279,6 +278,10 @@ APP.Main = (function() {
 
       // Now figure out how wide it is and use that to saturate it.
       scoreLocation = score.getBoundingClientRect();
+      var saturation = (100 * ((scoreLocation.width - 38) / 2));
+
+      score.style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
+      title.style.opacity = opacity;
     }
   }
 
