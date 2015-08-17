9f3bd5f0cf281dc01cded86e05713711a2e43aa4
hassan <hassanabdulqayyum@hotmail.com>
2015-08-17 18:26:20 +0500

    fixed typo

--- js/main.js
+++ js/main.js
@@ -504,7 +504,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   var pre = Math.sin((document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
-    var phase = pre + (i % 5));
+    var phase = pre + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
