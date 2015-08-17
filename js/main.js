64ec165b7ad36fa5e91d4ee3c50dc967300003b8
hassan <hassanabdulqayyum@hotmail.com>
2015-08-17 18:11:11 +0500

    added more log

--- js/main.js
+++ js/main.js
@@ -502,6 +502,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  console.log("start");
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     console.log(phase);
