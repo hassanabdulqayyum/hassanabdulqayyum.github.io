886263039e9cb13efd37e0127c82d2f51261530d
hassan <hassanabdulqayyum@hotmail.com>
2015-08-17 18:27:30 +0500

    fixed second typo

--- js/main.js
+++ js/main.js
@@ -502,7 +502,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var pre = Math.sin((document.body.scrollTop / 1250);
+  var pre = Math.sin(document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
     var phase = pre + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
