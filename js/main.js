dc20789687da1289585362d2d82f353efcafff9d
hassan <hassanabdulqayyum@hotmail.com>
2015-08-17 20:09:38 +0500

    exp tranform

--- js/main.js
+++ js/main.js
@@ -505,7 +505,7 @@ function updatePositions() {
   var pre = (document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin(pre + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.transform = "translateX(10)";
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
